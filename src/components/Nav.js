cc265b2081eeb90a4f0b7ded2e300943561cf64a
OhRaekyu <dhforb1234@naver.com>
2022-03-18 16:30:20 +0900

    :memo: 네비게이션 UI 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-const Nav = () => {
-  return <div>Nav</div>;
-};
+export default function Nav() {
+  const [show, setShow] = useState(false);
 
-export default Nav;
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    // return () => {
+    //   window.addEventListener('scroll', () => {});
+    // };
+  }, []);
+
+  return (
+    <NavWrap className={show && 'nav-black'}>
+      <img
+        alt="Netflix Logo"
+        src="//upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png"
+        onClick={window.location.reload}
+      />
+      <img
+        alt="User Profile"
+        src="/images/profile.png"
+        className="nav-profile"
+      />
+    </NavWrap>
+  );
+}
+
+const NavWrap = styled.nav`
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 30px;
+  padding: 15px 0;
+  z-index: 1;
+  transition: all 0.5s ease-in;
+
+  img {
+    margin: 0 60px;
+    cursor: pointer;
+    height: 30px;
+  }
+
+  &.nav-black {
+    background-color: rgb(20, 20, 20);
+  }
+
+  .nav-profile {
+    border-radius: 5px;
+  }
+`;
